43a50b6ecd46d9c634e0b3724cfa34a206404379
501A <art71903design@gmail.com>
2024-12-15 00:51:35 -0800

    Refactor Navbar to use Logo component and improve layout

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { DarkModeSwitch } from "./SwitchButton";
 import { useTheme } from "./ThemeContext";
+import Logo from "./Logo";
 
 const navbarLinks =
   "ml-4 p-1.5 no-underline text-black dark:text-white font-semibold";
@@ -8,9 +9,11 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   return (
     <nav className="mx-auto flex max-w-[600px] items-center py-6">
-      <div className="flex align-middle">
-        <object data="./src/images/shiori-logo.svg" className="mr-2 w-8" />
-        <h1 className="text-2xl font-bold text-black dark:text-white">
+      <div className="flex items-center bg-transparent transition-all dark:hover:bg-neutral-800 p-1.5 pr-4 pl-3 rounded-xl cursor-pointer active:scale-95">
+        <div className="w-fit h-fit rounded-full dark:text-yellow-500 text-3xl">
+          <Logo/>
+        </div>
+        <h1 className="ml-2 text-xl font-bold text-black dark:text-white select-none">
           Shiori
         </h1>
       </div>
