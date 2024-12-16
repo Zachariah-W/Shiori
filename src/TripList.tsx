7fda9bac1493c4f6ec2892f1676ca0302efcd2a1
501A <art71903design@gmail.com>
2024-12-16 10:23:32 -0800

    Refactor trip item style

--- src/TripList.tsx
+++ src/TripList.tsx
@@ -10,6 +10,7 @@ const TripList = ({ trips }: { trips: FirestoreTrip[] }) => {
     <div className="grid grid-cols-2 gap-3">
       {trips.map((trip, i) => (
         <motion.div
+         className="relative"
           key={i}
           initial={{ y: 100, opacity: 0, rotateZ: 8 }}
           animate={{ y: 0, opacity: 1, rotateZ: 0 }}
@@ -20,59 +21,54 @@ const TripList = ({ trips }: { trips: FirestoreTrip[] }) => {
             damping: 20,
           }}
         >
-          <div className="rounded-xl border border-b-2 border-neutral-200 bg-dotted px-6 py-4 text-left transition-all duration-300 hover:bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-700">
-            <Link className="no-underline" to={`/trip/${trip.id}`}>
-              <div className="flex h-16 items-center justify-between">
-                <div>
-                  <h2 className="flex items-center gap-2 text-xl font-semibold text-black dark:text-white">
-                      {trip.title}
-                      <p>|</p>
-                    {trip.country}
-                    {lookup.byCountry(trip.country)?.internet !=
-                      undefined && (
-                      <ReactCountryFlag
-                        countryCode={`${
-                          lookup.byCountry(trip.country)?.internet
-                        }`}
-                        style={{
-                          fontSize: "1.3em",
-                          paddingTop: "2px",
-                        }}
-                        aria-label={trip.country}
-                      />
-                    )}
-                  </h2>
+          <img
+            src={trip.image.urls.regular}
+            className="absolute rounded-xl bg-cover bg-center h-48 border border-b-2 border-neutral-200 p-1 text-left transition-all duration-300 hover:bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-700 top-0 left-0 w-full"
+            // className="rounded-xl border border-b-2 border-neutral-200 px-6 py-4 text-left transition-all duration-300 hover:bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-700"
+          />
+          <Link className="absolute no-underline bg-gradient-to-tr from-neutral-800 via-transparent to-transparent h-48 w-full top-0 left-0 rounded-b-xl flex items-end p-6" to={`/trip/${trip.id}`}>
+            <div className="w-fit h-fit">
+              <div>
+                <h2 className="flex items-center gap-2 text-xl font-semibold text-black dark:text-white">
+                    {trip.title}
+                    <p>|</p>
+                  {trip.country}
+                  {lookup.byCountry(trip.country)?.internet !=
+                    undefined && (
+                    <ReactCountryFlag
+                      countryCode={`${
+                        lookup.byCountry(trip.country)?.internet
+                      }`}
+                      style={{
+                        fontSize: "1.3em",
+                        paddingTop: "2px",
+                      }}
+                      aria-label={trip.country}
+                    />
+                  )}
+                </h2>
 
-                  <p className="text-black dark:text-white">
-                    Date:{" "}
-                    {trip.startDate &&
-                      format(
-                        trip.startDate instanceof Date
-                          ? trip.startDate
-                          : trip.startDate.toDate(),
-                        "MM/dd/yyyy",
-                      )}{" "}
-                    ~{" "}
-                    {trip.endDate &&
-                      format(
-                        trip.endDate instanceof Date
-                          ? trip.endDate
-                          : trip.endDate.toDate(),
-                        "MM/dd/yyyy",
-                      )}
-                  </p>
-                </div>
-                {trip.image && (
-                  <div
-                    className="h-full w-1/2 rounded-md bg-cover bg-center"
-                    style={{
-                      backgroundImage: `url(${trip.image.urls.regular})`,
-                    }}
-                  ></div>
-                )}
+                <p className="text-black dark:text-white">
+                  Date:{" "}
+                  {trip.startDate &&
+                    format(
+                      trip.startDate instanceof Date
+                        ? trip.startDate
+                        : trip.startDate.toDate(),
+                      "MM/dd/yyyy",
+                    )}{" "}
+                  ~{" "}
+                  {trip.endDate &&
+                    format(
+                      trip.endDate instanceof Date
+                        ? trip.endDate
+                        : trip.endDate.toDate(),
+                      "MM/dd/yyyy",
+                    )}
+                </p>
               </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
         </motion.div>
       ))}
     </div>
