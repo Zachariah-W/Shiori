683f683441f031c91e13391e0a5c1bc7c20de62e
501A <art71903design@gmail.com>
2024-12-18 00:04:23 +0900

    Refactor tripe item style

--- src/TripList.tsx
+++ src/TripList.tsx
@@ -10,7 +10,7 @@ const TripList = ({ trips }: { trips: FirestoreTrip[] }) => {
     <div className="grid grid-cols-2 gap-3">
       {trips.map((trip, i) => (
         <motion.div
-          className="relative"
+          className="relative h-48"
           key={i}
           initial={{ y: 100, opacity: 0, rotateZ: 8 }}
           animate={{ y: 0, opacity: 1, rotateZ: 0 }}
@@ -20,21 +20,23 @@ const TripList = ({ trips }: { trips: FirestoreTrip[] }) => {
             stiffness: 200,
             damping: 20,
           }}
+          whileTap={{ scale: 0.95, transition: { duration: 0.05 } }}
+          // whileHover={{ scale: 1.01, transition: { duration: 0.05 } }}
         >
           {trip.image !== undefined && (
             <img
               src={trip.image.urls.regular}
-              className="absolute left-0 top-0 h-48 w-full rounded-xl border border-b-2 border-neutral-200 bg-cover bg-center p-1 text-left transition-all duration-300 hover:bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-700"
+              className="absolute left-0 top-0 h-48 w-full rounded-xl border border-b-2 border-neutral-300 bg-cover bg-center shadow-md text-left transition-all duration-300 bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800"
             />
           )}
 
           <Link
-            className="absolute left-0 top-0 flex h-48 w-full items-end rounded-b-xl bg-gradient-to-tr from-neutral-800 via-transparent to-transparent p-6 no-underline"
+            className="absolute left-0 top-0 flex h-48 w-full items-end rounded-b-xl bg-gradient-to-t from-neutral-700 dark:from-neutral-800 via-transparent to-transparent p-6 no-underline"
             to={`/trip/${trip.id}`}
           >
             <div className="h-fit w-fit">
               <div>
-                <h2 className="flex items-center gap-2 text-xl font-semibold text-black dark:text-white">
+                <h2 className="flex items-center gap-2 text-xl font-semibold text-white">
                   {trip.title}
                   <p>|</p>
                   {trip.country}
@@ -52,7 +54,7 @@ const TripList = ({ trips }: { trips: FirestoreTrip[] }) => {
                   )}
                 </h2>
 
-                <p className="text-black dark:text-white">
+                <p className="text-white/60 font-normal">
                   Date:{" "}
                   {trip.startDate &&
                     format(
