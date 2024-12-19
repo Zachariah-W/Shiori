8d731b71e26097bca9ce25e55512f761c675ac20
Zachariah-W <zachwangs@gmail.com>
2024-12-19 11:54:11 -0800

    Restyle new trip button

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import LogoAnimation from "./brand/LogoAnimation";
 import shioriLogo from "./brand/shiori-logo-animation.json";
 
-const navbarLinks =
-  "ml-4 p-1.5 no-underline text-black dark:text-white font-semibold";
 const Navbar = () => {
   return (
     <nav className="w-layout flex items-center justify-between py-3">
-      <Link to="/Home" className="-ml-2 flex cursor-pointer items-center rounded-xl bg-transparent p-2 pl-3 pr-4 transition-all hover:bg-neutral-100 active:scale-100 dark:hover:bg-neutral-800 hover:scale-105">
+      <Link
+        to="/Home"
+        className="-ml-2 flex cursor-pointer items-center rounded-xl bg-transparent p-2 pl-3 pr-4 transition-all hover:scale-105 hover:bg-neutral-100 active:scale-100 dark:hover:bg-neutral-800"
+      >
         <div className="h-fit w-fit rounded-full text-3xl text-orange-500 dark:text-orange-500">
           <LogoAnimation src={shioriLogo}>
             <h1 className="ml-2 select-none text-xl font-semibold text-black dark:text-white">
@@ -17,7 +18,10 @@ const Navbar = () => {
         </div>
       </Link>
       <div className="ml-auto flex items-center gap-2.5">
-        <Link to="/create" className={navbarLinks}>
+        <Link
+          to="/create"
+          className="cursor-pointer rounded-xl p-2 font-semibold text-black no-underline transition-all hover:scale-105 hover:bg-neutral-100 active:scale-100 dark:text-white dark:hover:bg-neutral-800"
+        >
           New Trip
         </Link>
       </div>
