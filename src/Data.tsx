37301e784b01fb4c775f0330a71a849bdbf0c55e
Zachariah-W <zachwangs@gmail.com>
2024-12-21 16:40:30 -0800

    Add top country and num countries visted

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { FirestoreTrip } from "./Home";
 import { auth, db } from "../firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 
 const Data = () => {
-  const [dataCollection, setDataCollection] = useState<FirestoreTrip[]>([]);
+  const [countryVistNum, setCountryVistNum] = useState<Map<string, number>>(
+    new Map(),
+  );
+  const [topVists, setTopVisits] = useState<[string, number][]>([]);
 
   const getData = async () => {
     const tripsRef = collection(
@@ -14,24 +16,51 @@ const Data = () => {
       `trips`,
     );
     const tripsSnap = await getDocs(tripsRef);
+    const tempVistNumMap: Map<string, number> = new Map();
     if (!tripsSnap.empty) {
-      const tempTripsArray: FirestoreTrip[] = [];
-      tripsSnap.forEach((doc) => {
-        tempTripsArray.push({ ...doc.data(), id: doc.id } as FirestoreTrip);
+      tripsSnap.forEach((trip) => {
+        tempVistNumMap.set(
+          trip.data().country,
+          (tempVistNumMap.get(trip.data().country) || 0) + 1,
+        );
       });
-      setDataCollection(tempTripsArray);
+      setCountryVistNum(tempVistNumMap);
     }
+    const tempTopVistMap = Array.from(countryVistNum.entries()).sort(
+      (a, b) => b[1] - a[1],
+    );
+    setTopVisits(tempTopVistMap);
   };
+
   useEffect(() => {
     getData();
-    console.log(dataCollection);
   }, []);
 
   return (
-    <div className="w-layout flex h-52 items-center justify-center rounded-xl border border-neutral-200 bg-neutral-100 p-6 dark:border-neutral-700/50 dark:bg-neutral-800/50">
-      <h2 className="text-center text-lg font-medium text-neutral-500">
-        No Data found
-      </h2>
+    <div className="w-layout flex h-full items-center justify-center rounded-xl border border-neutral-200 bg-neutral-100 p-6 dark:border-neutral-700/50 dark:bg-neutral-800/50">
+      {}
+      <div className="w-layout flex h-80 flex-row items-center justify-between border border-red-500 px-8">
+        <div className="h-72 w-72 rounded-xl border-2 border-neutral-700 p-2">
+          <h2 className="pb-4 pt-3 text-center text-2xl font-bold">
+            Top Countries
+          </h2>
+          <ol className="text-lg *:font-semibold">
+            {topVists.slice(0, 3).map((data) => (
+              <li>
+                {data[0]}: {data[1]} times
+              </li>
+            ))}
+          </ol>
+        </div>
+        <div className="h-72 w-72 rounded-xl border-2 border-neutral-700 p-2">
+          <h2 className="pb-4 pt-3 text-center text-2xl font-bold">
+            Advanced Stats
+          </h2>
+          <ul className="text-lg *:font-semibold">
+            <li># of Countries Traveled: {countryVistNum.size}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
