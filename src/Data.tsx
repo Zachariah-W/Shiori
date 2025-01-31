26b75b26e64d3d2ad58d231f910c43c540a8bf1e
501A <art71903design@gmail.com>
2025-01-31 01:12:44 -0800

    Refactor chart styling

--- src/Data.tsx
+++ src/Data.tsx
@@ -5,8 +5,8 @@ import {
   Bar,
   BarChart,
   CartesianGrid,
-  XAxis,
   ResponsiveContainer,
+  YAxis,
 } from "recharts";
 import {
   ChartConfig,
@@ -68,7 +68,7 @@ const Data = () => {
   );
 
   return (
-    <div className="w-layout mb-2 flex h-full min-h-52 flex-col items-center justify-center gap-2 rounded-xl border border-neutral-200 bg-neutral-100 px-6 dark:border-neutral-700/50 dark:bg-neutral-800/50">
+    <div className="w-layout">
       {loading ? (
         <h2 className="text-center text-lg font-medium text-neutral-500">
           Loading trips...
@@ -79,48 +79,42 @@ const Data = () => {
         </h2>
       ) : (
         <>
-          <div className="w-layout flex h-80 flex-row items-center justify-between px-10">
-            <div className="h-64 w-72 rounded-xl border-2 border-neutral-700 bg-graph p-2">
-              <h2 className="py-3 text-center text-2xl font-bold">
-                Top Countries
-              </h2>
-              <ol className="text-lg font-semibold">
-                {topVists.slice(0, 5).map((data, index) => (
+          <div className="w-layout grid grid-cols-3 gap-4">
+            <section className="col-span-2">
+              <h2 className="p-4">Trip Statistics</h2>
+              <div className="h-fit w-full overflow-clip rounded-2xl border border-neutral-200 bg-white p-4 shadow-sm dark:border-neutral-800 dark:bg-neutral-900">
+                <ChartContainer config={chartConfig} className="min-h-10">
+                  <ResponsiveContainer>
+                    <BarChart
+                      data={transformedData}
+                      layout="vertical"
+                      className="-mr-12"
+                    >
+                      <CartesianGrid horizontal={false} />
+                      <YAxis
+                        dataKey="Country"
+                        type="category"
+                        tickLine={false}
+                        tickMargin={10}
+                        axisLine={false}
+                      />
+                      <ChartTooltip content={<ChartTooltipContent />} />
+                      <Bar dataKey="VisitCount" fill="#ff5722" radius={8} />
+                    </BarChart>
+                  </ResponsiveContainer>
+                </ChartContainer>
+              </div>
+            </section>
+            <section>
+              <h2 className="p-4">Top Countries</h2>
+              <ol className="list-inside list-decimal px-4">
+                {topVists.slice(0, 5).map(([countryName, count], index) => (
                   <li key={index}>
-                    {data[0]}: {data[1]} times
+                    {countryName}: {count} times
                   </li>
                 ))}
               </ol>
-            </div>
-            <div className="h-64 w-72 rounded-xl border-2 border-neutral-700 bg-graph p-2">
-              <h2 className="py-3 text-center text-2xl font-bold">
-                Advanced Stats
-              </h2>
-              <ul className="text-lg font-semibold">
-                <li># of Countries Traveled: {countryVistNum.size}</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="w-layout h-fit rounded-lg px-10">
-            <ChartContainer
-              config={chartConfig}
-              className="rounded-lg border-2 border-neutral-700"
-            >
-              <ResponsiveContainer width="100%" height={300}>
-                <BarChart data={transformedData}>
-                  <CartesianGrid vertical={false} />
-                  <XAxis
-                    dataKey="Country"
-                    tickLine={false}
-                    tickMargin={10}
-                    axisLine={false}
-                  />
-                  <ChartTooltip content={<ChartTooltipContent />} />
-                  <Bar dataKey="VisitCount" fill="#ff5722" radius={15} />
-                </BarChart>
-              </ResponsiveContainer>
-            </ChartContainer>
+            </section>
           </div>
         </>
       )}
